32143aa92b414435b0528f0f32521281443f8441
amito07 <lianbadhon@gmail.com>
2023-07-16 02:13:02 +0600

    call API in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import { useEffect, useState } from "react";
 import FoodCard from "@/components/FoodCard";
 import Set from "@/components/Set";
 import Fooddata from "@/data/foodlist";
+import { useRouter } from "next/router";
 import PublicLayout from "../components/PublicLayout";
 
 export default function Home() {
   const [fdata, setFdata] = useState(Fooddata);
   const [copydata, setCopyData] = useState([]);
-
+  const route = useRouter();
   const searcher = new FuzzySearch(Fooddata, ["rname"], {
     caseSensitive: false,
   });
@@ -21,10 +22,18 @@ export default function Home() {
     setCopyData(result);
   };
 
+  const getFoodItems = async () => {
+    const res = await fetch(`${process.env.BASE_URL}/api/restaurants`, {
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    setCopyData(data);
+  };
+  const handleCardClick = (id) => {
+    route.push(`/restaurant/${id}`);
+  };
   useEffect(() => {
-    setTimeout(() => {
-      setCopyData(Fooddata);
-    }, 3000);
+    getFoodItems();
   }, []);
 
   return (
@@ -48,9 +57,9 @@ export default function Home() {
             onChange={(e) => handleSearch(e.target.value)}
           />
         </Grid>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} style={{ cursor: "pointer" }}>
           {copydata && copydata.length ? (
-            <FoodCard item={copydata} />
+            <FoodCard item={copydata} handleCardClick={handleCardClick} />
           ) : (
             <Set sdata={fdata} />
           )}
